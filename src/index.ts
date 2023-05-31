6a664559bbe3f8e516d955d51b7d130cd7dbb2a8
DolanPump <nickvanrossenberg@hotmail.com>
2023-05-31 11:59:51 +0200

    get account stuff

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,25 @@
 import { createHmac } from 'crypto';
 import { AuthHeader } from './types/auth';
 import querystring from 'querystring';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 export class Client {
-  private baseUrl = 'https://api.kucoin.com';
+  private baseUrl = 'https://api.kucoin.com/api';
+  // private baseUrl = 'https://openapi-sandbox.kucoin.com';
 
   constructor(private secret: string, private password: string, private key: string) {}
 
-  sign(text: string, secret: string) {
+  private sign(text: string, secret: string) {
     return createHmac('sha256', secret).update(text).digest('base64');
   }
 
-  createAuth(method: string, url: string, body?: object): AuthHeader {
+  private createAuth(method: string, url: string, body?: object): AuthHeader {
     const bodyToSend = body ? JSON.stringify(body) : '';
     const timestamp = Date.now().toString();
+    console.log({ timestamp });
     const signature = this.sign(timestamp + method.toUpperCase() + url + bodyToSend, this.secret);
     const passphrase = this.sign(this.password, this.secret);
     return {
@@ -30,11 +35,22 @@ export class Client {
     };
   }
 
-  async getSubAccounts(params: IGetSubAccounts) {
-    const url = `${this.baseUrl}/api/v2/sub/user?` + querystring.stringify(params);
-    console.log({ url });
-    return axios.get<IResGetSubAccounts>(url, this.createAuth('GET', url));
-  }
+  private get = (endpoint: string, params?: any) => {
+    const url = `${this.baseUrl}${endpoint}?${querystring.stringify(params)}`;
+    return axios.get(url, this.createAuth('GET', url));
+  };
+  private post = (endpoint: string, body: any) => {
+    const url = `${this.baseUrl}${endpoint}`;
+    return axios.post(url, body, this.createAuth('POST', url, body));
+  };
+
+  getSubAccounts = async (params: IGetSubAccounts) => this.get(`/v2/sub/user`, params);
+  listAccounts = async (params: IListAccounts) => this.get(`/v1/accounts`, params);
+  getAnAccount = async ({ accountId }: IGetAnAccount) => this.get(`/v1/accounts/${accountId}`);
+  getAccountLedgers = async (params: IGetAccountLedgers) =>
+    this.get(`/v1/accounts/ledgers`, params);
+  getAccountSummaryInfo = async () => this.get(`/v2/user-info`);
+  createSubAccount = async (body: ICreateSubAccount) => this.post(`/v2/sub/user/created`, body);
 }
 
 type IGetSubAccounts = {
@@ -42,12 +58,46 @@ type IGetSubAccounts = {
   pageSize?: number;
 };
 
-type IResGetSubAccounts = {
-  currentPage: number;
-  pageSize: number;
-  totalNum: number;
-  totalPage: number;
-  items: any[];
+type IListAccounts = {
+  currency?: string;
+  type?: 'main' | 'trade' | 'margin';
 };
 
-async function getSubAccountsTest(authHeader: AuthHeader) {}
+type IGetAnAccount = {
+  accountId: string;
+};
+
+type IGetAccountLedgers = {
+  currency?: string;
+  direction?: 'in' | 'out';
+  bizType?:
+    | 'DEPOSIT'
+    | 'WITHDRAW'
+    | 'TRANSFER'
+    | 'SUB_TRANSFER'
+    | 'TRADE_EXCHANGE'
+    | 'MARGIN_EXCHANGE'
+    | 'KUCOIN_BONUS';
+  startAt?: string;
+  endAt?: string;
+};
+
+type ICreateSubAccount = {
+  password: string;
+  remarks?: string;
+  subName: string;
+  access: 'Spot' | 'Futures' | 'Margin';
+};
+
+async function test() {
+  const client = new Client(
+    process.env.SECRET as string,
+    process.env.PASS as string,
+    process.env.KEY as string,
+  );
+  console.log(client);
+  const { data } = await client.getAccountSummaryInfo();
+  console.log(data);
+}
+
+test();
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -6,7 +6,6 @@ dotenv.config();
 
 describe('Create client and check user info', () => {
   it('should create a client', async () => {
-    console.log(process.env.SECRET);
     const client = new Client(
       process.env.SECRET as string,
       process.env.PASS as string,
@@ -14,10 +13,14 @@ describe('Create client and check user info', () => {
     );
 
     console.log(client);
-    const { data } = await client.getSubAccounts({
-      currentPage: 1,
-      pageSize: 1,
-    });
-    console.log(data);
+    // const data = await client.getSubAccounts({
+    //   currentPage: 1,
+    //   pageSize: 1,
+    // });
+
+    // const { data } = await client.listAccounts({});
+    // console.log(data);
+
+    const hi = await client.getAccountSummaryInfo();
   });
 });
